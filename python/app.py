eb6a14dec1c885236b8130ed9635562d0b0b9074
Romi Sugianto <romisugianto97@gmail.com>
2024-04-27 14:56:08 +0700

    docs: add more command

--- python/app.py
+++ python/app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get('NAME', '')
+    name = os.environ.get('NAME', 'flask')
     return f"<p>Hello, World! from {name}</p>"
