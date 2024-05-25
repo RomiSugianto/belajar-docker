e6ae33b02348d1fe5a64788b3f87b94410c84904
Romi Sugianto <romisugianto97@gmail.com>
2024-05-26 00:19:26 +0700

    fix: some pylint

--- docker compose/flask-redis/app.py
+++ docker compose/flask-redis/app.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask, request, jsonify
 import redis
-import os
 
 app = Flask(__name__)
 redis_host = os.environ.get('REDIS_HOST', 'localhost')
@@ -9,15 +9,18 @@ redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_respon
 
 @app.route('/')
 def home():
+    """root"""
     return "Welcome to the Flask-Redis App!"
 
 @app.route('/set/<key>/<value>', methods=['GET'])
 def set_value(key, value):
+    """to set key value to redis"""
     redis_client.set(key, value)
     return f"Set {key} to {value}"
 
 @app.route('/get/<key>', methods=['GET'])
 def get_value(key):
+    """function for get value from redis"""
     value = redis_client.get(key)
     if value:
         return jsonify({key: value})
